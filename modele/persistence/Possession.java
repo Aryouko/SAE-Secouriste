afbdc80a18d8fd6adeafee3ae459394527b85d60
Célian <brocart.celian@gmail.com>
2025-03-18 12:30:43 +0100

    possession

--- modele/persistence/Possession.java
+++ modele/persistence/Possession.java
@@ -3,4 +3,43 @@ package modele.persistence;
 /**
  * This class represents a possession.
  */
-public class Possession {}
+public class Possession {
+
+    /**
+     * The possession's identifier.
+     */
+    public Journee journeeDisp; // journeeDisp
+    private Secouriste secouristeDisp; // secouristeDisp
+
+    /**
+     * Getter for the possession's identifier.
+     * @return The possession's identifier.
+     */
+    public Journee getJourneeDisp() {
+        return journeeDisp;
+    }
+
+    /**
+     * Setter for the possession's identifier.
+     * @param journeeDisp The possession's identifier.
+     */
+    public void setJourneeDisp(Journee journeeDisp) {
+        this.journeeDisp = journeeDisp;
+    }
+
+    /**
+     * Getter for the possession's identifier.
+     * @return The possession's identifier.
+     */
+    public Secouriste getSecouristeDisp() {
+        return secouristeDisp;
+    }
+
+    /**
+     * Setter for the possession's identifier.
+     * @param secouristeDisp The possession's identifier.
+     */
+    public void setSecouristeDisp(Secouriste secouristeDisp) {
+        this.secouristeDisp = secouristeDisp;
+    }
+}
