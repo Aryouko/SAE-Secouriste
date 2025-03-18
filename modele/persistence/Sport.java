d3036acc959560841667abe72ada6f629089adbe
Célian <brocart.celian@gmail.com>
2025-03-18 11:57:06 +0100

    push

--- modele/persistence/Sport.java
+++ modele/persistence/Sport.java
@@ -4,11 +4,35 @@ public class Sport {
     String code;
     String nom;
 
+    /**
+     * Getter of Code
+     * @return the code of the sport
+     */
     public String getCode() {
         return this.code;
     }
 
+    /**
+     * Getter of Nom
+     * @return the name of the sport
+     */
     public String getNom() {
         return this.nom;
+    }
+
+    /**
+     * Setter of Code
+     * @param code the code of the sport to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Setter of Nom
+     * @param nom the name of the sport to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 }
