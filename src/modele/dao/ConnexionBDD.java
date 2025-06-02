b3f8225302a8c7eb108cc58a875fffaf36c95f2e
Célian <brocart.celian@gmail.com>
2025-06-02 10:41:43 +0200

    DAO implementation / creation ConnexionDao + secouriste findall

--- src/modele/dao/ConnexionBDD.java
+++ src/modele/dao/ConnexionBDD.java
@@ -5,11 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnexionBDD {
+    private static final String user = "admin";
+    private static final String password = "admin";
+    private static final String url = "jdbc:mysql://server.sauveteur:3306/SAE";
+
     private static Connection connexion;
 
+
     public static Connection getConnexion() throws SQLException {
+        System.out.println("Connexion en cours");
         if (connexion == null || connexion.isClosed()) {
-            connexion = DriverManager.getConnection("jdbc:mysql://server.sauveteur:3306/SAE", "admin", "admin");
+            connexion = DriverManager.getConnection( url, user, password);
         }
         return connexion;
     }
---
+++ src/view/MainApp.java
@@ -0,0 +1,29 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    /**
+     * Start methode of the APP
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = new FXMLLoader(this.getClass().getResource("view.fxml")).load();
+        primaryStage.setTitle("Dice Roller");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
