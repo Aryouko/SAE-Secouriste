82afa4ac2e64527e2734c2b28532a818250026c3
Célian <brocart.celian@gmail.com>
2025-06-03 12:24:34 +0200

    j'avance

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -10,15 +10,15 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MotDePasseOublie.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Inscription.fxml"));
 
-
+        
         javafx.scene.text.Font.loadFont(
                 getClass().getResourceAsStream("/fonts/JetBrainsMono-Regular.ttf"), 19
         );
 
         stage.setTitle("SAE Secouriste");
-        stage.setScene(new Scene(root, 400, 423));
+        stage.setScene(new Scene(root, 1920, 1080));
         stage.show();
     }
 
