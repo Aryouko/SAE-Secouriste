62faa1559174bc697b45d1b17ee5a43ad9a83a08
c0w5lip <152532580+c0w5lip@users.noreply.github.com>
2025-06-04 20:59:34 +0200

    remove bs

--- src/model/dao/ConnexionBDD.java
+++ src/model/dao/ConnexionBDD.java
@@ -4,17 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static model.utils.FetchDatabaseCredentials.*;
+
 public class ConnexionBDD {
-    private static final String username = "root";
-    private static final String password = "azerty";
-    private static final String url = "";
+    private static final String username = getUsername();
+    private static final String password = getPassword();
+    private static final String url = getUrl();
     private static Connection connexion;
 
 
     public static Connection getConnexion() throws SQLException {
         System.out.println("Connexion en cours");
         if (connexion == null || connexion.isClosed()) {
-            connexion = DriverManager.getConnection( url, username, password);
+            connexion = DriverManager.getConnection(url, username, password);
         }
         return connexion;
     }
