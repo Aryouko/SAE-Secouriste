ceb93a5b8bade3654a4df92ec0c46adb738645bb
Célian <brocart.celian@gmail.com>
2025-06-05 15:55:11 +0200

    Controller des pages de connexions clean avec un utilsController

--- src/controller/ConnexionController.java
+++ src/controller/ConnexionController.java
@@ -1,12 +1,10 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-
-import java.io.IOException;
-
+import static controller.UtilsController.linkToPage;
+import static controller.UtilsController.showError;
 import static model.data.service.AuthentificationManagement.getInstanceAuthentificationManagement;
 
 public class ConnexionController {
@@ -25,75 +23,21 @@ public class ConnexionController {
 
     @FXML
     public void linkToRegister() {
-        try {
-            // Charger la page Inscription.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Inscription.fxml"));
-            AnchorPane pageInscription = loader.load();
-
-            // Remplacer le contenu actuel par la nouvelle page
-            pageConnexion.getChildren().setAll(pageInscription);
-
-            // Optionnel : si tu veux adapter la taille ou ancrer la nouvelle page
-            AnchorPane.setTopAnchor(pageInscription, 0.0);
-            AnchorPane.setBottomAnchor(pageInscription, 0.0);
-            AnchorPane.setLeftAnchor(pageInscription, 0.0);
-            AnchorPane.setRightAnchor(pageInscription, 0.0);
-        } catch (IOException e) {
-            System.err.println("Erreur lors du chargement de la page Inscription.fxml : " + e.getMessage());
-        }
+        linkToPage(pageConnexion, "/fxml/Inscription.fxml");
     }
 
     @FXML
     public void linkToReceiveCode() {
-        try {
-            // Charger la page Inscription.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/EnvoiCode.fxml"));
-            AnchorPane pageMotDePasseOublie = loader.load();
-
-            // Remplacer le contenu actuel par la nouvelle page
-            pageConnexion.getChildren().setAll(pageMotDePasseOublie);
-
-            // Optionnel : si tu veux adapter la taille ou ancrer la nouvelle page
-            AnchorPane.setTopAnchor(pageMotDePasseOublie, 0.0);
-            AnchorPane.setBottomAnchor(pageMotDePasseOublie, 0.0);
-            AnchorPane.setLeftAnchor(pageMotDePasseOublie, 0.0);
-            AnchorPane.setRightAnchor(pageMotDePasseOublie, 0.0);
-
-        } catch (IOException e) {
-            System.err.println("Erreur lors du chargement de la page ReceiveCode.fxml : " + e.getMessage());
-        }
+        linkToPage(pageConnexion, "/fxml/EnvoiCode.fxml");
     }
-
 
     @FXML
     public void ButtonConnexionClicked() {
-        // Logique de connexion ici
         System.out.println("Connexion button clicked");
         if (getInstanceAuthentificationManagement().login(mailTextField.getText(), passwordPasswordField.getText())) {
-            System.out.println("login et mdp bons");
-            try {
-                // Load Evenement
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FenetreEvenement.fxml"));
-                AnchorPane pageEvenement = loader.load();
-
-                // Remplacer le contenu actuel par la nouvelle page
-                pageConnexion.getChildren().setAll(pageEvenement);
-
-                AnchorPane.setTopAnchor(pageEvenement, 0.0);
-                AnchorPane.setBottomAnchor(pageEvenement, 0.0);
-                AnchorPane.setLeftAnchor(pageEvenement, 0.0);
-                AnchorPane.setRightAnchor(pageEvenement, 0.0);
-
-            } catch (IOException e) {
-                System.err.println("Erreur lors du clique bouton connexion : " + e.getMessage());
-            }
+            linkToPage(pageConnexion, "/fxml/FenetreEvenement.fxml");
         } else {
-            // Afficher le pop-up d'inscription réussie
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Mauvais identifiant");
-            alert.setHeaderText(null);
-            alert.setContentText("Mauvais identifiant ! \n Vérifier les identifier ");
-            alert.showAndWait();
+            showError("Identifiant ou mot de passe incorrect. Veuillez réessayer.");
         }
     }
 }
--- src/controller/InscriptionController.java
+++ src/controller/InscriptionController.java
@@ -1,19 +1,15 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import model.data.service.AuthentificationManagement;
 
-import java.io.IOException;
 import java.sql.SQLException;
 
-import static controller.Alerte.showError;
-import static controller.Alerte.showInfo;
+import static controller.UtilsController.linkToPage;
+import static controller.UtilsController.showError;
+import static controller.UtilsController.showInfo;
 import static model.data.service.AuthentificationManagement.getInstanceAuthentificationManagement;
 
 public class InscriptionController {
@@ -29,10 +25,9 @@ public class InscriptionController {
 
     @FXML
     private AnchorPane pageRegister;
-    
+
     @FXML
     public void ButtonRegisterClicked() {
-        // Logique d'inscription ici
         try {
             System.out.println("Inscription button clicked");
             System.out.println("Email: " + labelMail.getText());
@@ -42,31 +37,13 @@ public class InscriptionController {
             } else {
                 showError("L'inscription a échoué. Veuillez vérifier vos informations.");
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             showError("Une erreur est survenue lors de l'inscription. Veuillez réessayer plus tard.");
         }
     }
 
     @FXML
     private void linkToConnexion() {
-        try {
-            // Charger la page Inscription.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Connexion.fxml"));
-            AnchorPane pageInscription = loader.load();
-
-            // Remplacer le contenu actuel par la nouvelle page
-            pageRegister.getChildren().setAll(pageInscription);
-
-            // Optionnel : si tu veux adapter la taille ou ancrer la nouvelle page
-            AnchorPane.setTopAnchor(pageInscription, 0.0);
-            AnchorPane.setBottomAnchor(pageInscription, 0.0);
-            AnchorPane.setLeftAnchor(pageInscription, 0.0);
-            AnchorPane.setRightAnchor(pageInscription, 0.0);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Erreur lors du chargement de la page Inscription.fxml");
-        }
+        linkToPage(pageRegister, "/fxml/Connexion.fxml");
     }
 }
