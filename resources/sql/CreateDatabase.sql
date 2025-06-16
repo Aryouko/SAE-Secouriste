56de3dcad3186ebe7e2f85dd28233cbb867c5f31
glenpotay <potay.2400485@etud.univ-ubs.fr>
2025-06-16 16:30:19 +0200

    commit

--- resources/sql/CreateDatabase.sql
+++ resources/sql/CreateDatabase.sql
@@ -47,7 +47,7 @@ CREATE TABLE Secouriste (
     date_naissance VARCHAR(32),
     tel VARCHAR(10),
     adresse VARCHAR(64),
-    photo BLOB(1000),
+    photo BLOB,
     CONSTRAINT pk_Secouriste PRIMARY KEY (idSecouriste),
     CONSTRAINT fk_Secouriste_User FOREIGN KEY (idSecouriste) REFERENCES User(idUser)
 );
