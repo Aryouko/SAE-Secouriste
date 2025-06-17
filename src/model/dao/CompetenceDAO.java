42409462887cbf90505ff795b2c10d2b924fb5ba
Célian <brocart.celian@gmail.com>
2025-06-17 11:27:33 +0200

    Affectation

--- src/model/dao/CompetenceDAO.java
+++ src/model/dao/CompetenceDAO.java
@@ -1,4 +1,33 @@
 package model.dao;
 
+import model.data.persistence.Competence;
+import model.data.persistence.Secouriste;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+
+import static model.dao.ConnectionBDD.getConnection;
+
 public class CompetenceDAO {
+
+    public List<String> findAllIntitule() {
+        List<String> competences = new ArrayList<>();
+
+        try (Connection con = getConnection();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM Competence")) {
+
+            while (rs.next()) {
+                String intitule = rs.getString("intitule");
+                competences.add(intitule);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return competences;
+    }
 }
