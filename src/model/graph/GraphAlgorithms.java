85a8bcd21079dc7cc6304dc3edd9500e6bb83b60
Célian <brocart.celian@gmail.com>
2025-06-18 17:26:01 +0200

    Assignment

--- src/model/graph/GraphAlgorithms.java
+++ src/model/graph/GraphAlgorithms.java
@@ -1,43 +1,54 @@
 package model.graph;
 
-
+/**
+ * GraphAlgorithms - This class provides utility methods for working with graphs,
+ * including checking if a graph is oriented, detecting cycles in a directed graph,
+ * and validating adjacency matrices.
+ */
 public class GraphAlgorithms {
 
 
+    /**
+     * It checks if the graph is oriented
+     * @param matrix - the adjacency matrix given
+     * @return true if the graph is oriented, false otherwise
+     */
     public static boolean isOriented(int[][] matrix) {
         int indexL = 0 ;
         for (int[] line : matrix) {
 
+            // For each line, we check if there is a column with a value of 1
             int indexC = 0;
             for (int column : line) {
 
+                // If the value is 1, we check if the corresponding column has a value of 1 in the opposite direction
                 if (column == 1 ) {
                     if ( matrix[indexC][indexL] == 1 ) {
-                        return false ;
+                        return false ; // If there is a value of 1 in the opposite direction, the graph is not oriented
                     }
                 }
-                indexC++;
+                indexC++; // Increment the column index
             }
-            indexL++;
+            indexL++; // Increment the line index
         }
         return true;
     }
 
 
     /**
-     * It checks if e cycle is detected during the DFS
-     * @param adjMatrix - the adjacency matrix given
+     * It checks if z cycle is detected during the DFS
+     * @param adjMatrix - the adjacency matrix
      * @param v - the vertex we are going to check
      * @param visited - the list that contains true if a vertex is visited, false otherwise
      * @param inProcess - the list that contains true if a vertex is going to be studied from the current vertex, false otherwise
      * @return true if a cycle is detected, false otherwise
      */
     public static boolean cycleDetected(int[][] adjMatrix, int v, boolean[] visited, boolean[] inProcess) {
-        visited[v] = true;
-        inProcess[v] = true;
+        visited[v] = true; // Mark the current vertex v as visited in the visited list
+        inProcess[v] = true; // Mark the vertex as in process
 
-        for (int i = 0; i < adjMatrix.length; i++) {
-            if (adjMatrix[v][i] == 1) {
+        for (int i = 0; i < adjMatrix.length; i++) { // Iterate through all vertices using the adjacency matrix
+            if (adjMatrix[v][i] == 1) { // Check if there is an edge from vertex v to vertex i
                 if (!visited[i]) {
                     if (cycleDetected(adjMatrix, i, visited, inProcess)) return true;
                 } else if (inProcess[i]) {
