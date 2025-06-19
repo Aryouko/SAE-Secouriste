ecb81a86b6400296d5645c7cdde33f7705c982b6
Célian <brocart.celian@gmail.com>
2025-06-19 18:03:28 +0200

    Push

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -13,7 +13,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/auth/Login.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/auth/LoadingScreen.fxml")));
 
         stage.getIcons().add(new Image("/images/logo.png"));
         stage.setTitle("SAE Secouriste");
