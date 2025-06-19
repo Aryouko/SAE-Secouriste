3adedd5e26ac3ba8902d06bb605e2f4df19169e8
Aryouko <carre.e2402145@etud.univ-ubs.fr>
2025-06-19 18:14:16 +0200

    C'est l'heure du commit

--- src/model/dao/AffectationDAO.java
+++ src/model/dao/AffectationDAO.java
@@ -52,7 +52,6 @@ public class AffectationDAO {
     public boolean rescuerThisDay(long idJournee, long idSecouriste) {
         boolean ret = false;
         String query = "SELECT 1 FROM Affectation a JOIN DPS d ON a.DPSAffect = d.id WHERE d.journee = ? AND a.secouristeAffect = ? LIMIT 1";
-        System.out.println("Query check: journee=" + idJournee + ", secouriste=" + idSecouriste);
 
         try (Connection con = ConnectionBDD.getConnection();
              PreparedStatement stmt = con.prepareStatement(query)) {
@@ -69,7 +68,6 @@ public class AffectationDAO {
             e.printStackTrace();
             ret = true;
         }
-        System.out.println("Return : " + ret);
         return ret;
     }
 
